4e609023de620c59289bfbe8caa3b08a4b45cb6b
Chris Kang <chris.v.kang@gmail.com>
2021-08-20 22:15:18 -0700

    added palette pan styles

--- src/components/Palette/PalettePan.js
+++ src/components/Palette/PalettePan.js
@@ -1,10 +1,28 @@
+import { useState } from "@hookstate/core"
+import classNames from "classnames"
 import "./PalettePan.css"
 
 export default function PalettePan({ name, hex, ...props }) {
+  const isHovered = useState(false)
+  const isActive = useState(false)
+
+  const panClassNames = classNames(
+    'palette__pan',
+    { 'palette__pan--hover': isHovered.get() },
+    { 'palette__pan--active': isActive.get() }
+  )
+
   return (
     <div 
-      className="palette__pan"
+      className={panClassNames}
       style={{ background: `#${hex}`}}
+      onMouseEnter={() => isHovered.set(true)}
+      onMouseLeave={() => {
+        isHovered.set(false)
+        isActive.set(false)
+      }}
+      onMouseDown={() => isActive.set(true)}
+      onMouseUp={() => isActive.set(false)}
       name={name}
       hex={hex}
       {...props}  
