7fd71b9c7ce7e9b14f81a50b8c3b5a8d69102fdd
Chris Kang <chris.v.kang@gmail.com>
2021-08-20 22:07:26 -0700

    moved palette files, removed pseudo elements

--- src/components/Palette/Palette.js
+++ src/components/Palette/Palette.js
@@ -1,19 +1,21 @@
-import PalettePan from '../PalettePan/PalettePan'
+import PalettePan from './PalettePan'
 import './Palette.css'
 
 export default function Palette({ pans, onPanClick }) {
 
   return (
     <div className="palette">
-      {pans.map(({ hex, name }, idx) => (
-        <PalettePan
-          key={`palette-pan-${idx}`}
-          name={name}
-          hex={hex}
-          onClick={onPanClick}
-        />
-      ))
-      }
+      <div className="palette__frame">
+        {pans.map(({ hex, name }, idx) => (
+          <PalettePan
+            key={`palette-pan-${idx}`}
+            name={name}
+            hex={hex}
+            onClick={onPanClick}
+          />
+        ))}
+      </div>
+    
     </div>
   )
 }
--- src/state/currentTray.js
+++ src/state/currentTray.js
@@ -1,10 +1,12 @@
-import { useState } from '@hookstate/core'
+import { none, useState } from '@hookstate/core'
 import globalState from './global'
 
 const wrapState = (s) => ({
   get: () => s.currentTrayIdx.value,
   set: (idx) => s.currentTrayIdx.set(idx),
-  reset: () => s.currentTrayIdx.set(-1)
+  reset: () => s.currentTrayIdx.set(-1),
+  addColor: (color) => s.trays[s.currentTrayIdx.value].merge(color),
+  removeColor: (idx) => s.trays[s.currentTrayIdx.value][idx].set(none),
 })
 
 export const useCurrentTrayState = () => wrapState(useState(globalState))
