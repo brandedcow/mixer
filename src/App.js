34aff46c4099c1b4cc0b7066b90c02a304ff5bb8
Chris Kang <chris.v.kang@gmail.com>
2021-08-30 15:53:43 -0700

    added padding to content

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Header from './components/Header/Header.js';
-import Mixer from './pages/Mixer/Mixer.js'
-import Colors from './pages/Colors/Colors.js';
-import Palettes from './pages/Palettes/Palettes'
-import './App.css'
+import Header from "./components/Header/Header.js";
+import Mixer from "./pages/Mixer/Mixer.js";
+import Colors from "./pages/Colors/Colors.js";
+import Palettes from "./pages/Palettes/Palettes";
+import About from "./pages/About/About";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Header title="Mixer" />
-        <Switch>
-          <Route path="/mixer" exact>
-            <Mixer />
-          </Route>
-          <Route path="/colors">
-            <Colors />
-          </Route>
-          <Route path="/palettes">
-            <Palettes />
-          </Route>
-        </Switch>
+        <div className="app__content">
+          <Switch>
+            <Route path="/mixer" exact>
+              <Mixer />
+            </Route>
+            <Route path="/colors">
+              <Colors />
+            </Route>
+            <Route path="/palettes">
+              <Palettes />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
       </div>
     </Router>
   );
